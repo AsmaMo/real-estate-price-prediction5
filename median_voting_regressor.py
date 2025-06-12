c5ce3be66fa59ad4ed272ea6c0c2c3a304e13f88
Asma <72828331+AsmaMo@users.noreply.github.com>
2025-06-12 17:26:03 +0300

    Add files via upload

---
+++ median_voting_regressor.py
@@ -0,0 +1,12 @@
+class MedianVotingRegressor:
+    def __init__(self, ...):
+        # التعريفات الخاصة بالكلاس
+        pass
+
+    def fit(self, X, y):
+        # fit method
+        pass
+
+    def predict(self, X):
+        # predict method
+        pass
