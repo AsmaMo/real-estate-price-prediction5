bc6ffffd432c8c729fdb49f8744a53edacd01c87
Asma <72828331+AsmaMo@users.noreply.github.com>
2025-06-12 17:45:42 +0300

    median_voting_regressor.py

--- median_voting_regressor.py
+++ median_voting_regressor.py
@@ -1,12 +1,18 @@
-class MedianVotingRegressor:
-    def __init__(self, ...):
-        # التعريفات الخاصة بالكلاس
-        pass
+import numpy as np
+from sklearn.base import RegressorMixin, BaseEstimator, clone
+
+class MedianVotingRegressor(RegressorMixin, BaseEstimator):
+    def __init__(self, estimators):
+        self.estimators = estimators
 
     def fit(self, X, y):
-        # fit method
-        pass
+        self.fitted_ = []
+        for name, est in self.estimators:
+            model = clone(est)
+            model.fit(X, y)
+            self.fitted_.append((name, model))
+        return self
 
     def predict(self, X):
-        # predict method
-        pass
+        predictions = np.column_stack([model.predict(X) for _, model in self.fitted_])
+        return np.median(predictions, axis=1)
